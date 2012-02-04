4bc4a09512ec3f39a81cf4e35fa6f9034d83c7c4
charless-c9 <charless.c9.test@gmail.com>
2012-02-04 15:11:18 +0000

    Try using express

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+  
 var port = process.env.PORT || 8001;
 
 app.listen(port);
 
-function handler (req, res) {
-  fs.readFile('index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
-    res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
-    res.end(data);
-  });
-}
 
 io.sockets.on('connection', function (socket) {
     // echo the message
